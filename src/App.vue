8905045d338a6f5559cfaef4c29cc69f94eeda9c
charlie <charliegogh@gmail.com>
2023-06-21 17:13:12 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <div>
-    <alert>
-      12312
-    </alert>
+    <star />
   </div>
 </template>
 <script>
-import alert from '../lib/alert'
+import star from '../lib/star'
 export default {
   components: {
-    alert
+    star
   },
   mounted() {
   }
